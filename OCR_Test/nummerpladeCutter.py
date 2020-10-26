d9a379e96edf7300a298ec7c48a1e1df85e69039
reptinias <32259405+reptinias@users.noreply.github.com>
2020-10-26 20:41:46 +0100

    Noget der ligner blod og bogstav crop

--- OCR_Test/nummerpladeCutter.py
+++ OCR_Test/nummerpladeCutter.py
@@ -1,12 +1,82 @@
 import cv2
 import numpy as np
+from collections import deque
 import pytesseract as pt
 
-#Load billede og konvater det til gray-scale
-img = cv2.imread('bilnummerplade7.jpg')
-grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+letterNumber = 0
 
-#Tilføj et blur filter
+def count(threshCrop):
+    visited = np.zeros((threshCrop.shape[1], threshCrop.shape[0]))
+    for y in range(0, threshCrop.shape[0]):
+        for x in range(0, threshCrop.shape[1]):
+            if resized[y][x] == 0 and visited[x][y] != 1:
+                append = True
+                XYArray = []
+                queue = deque([])
+                #nummerpladeFrame[y, x] = (0, 255, 0)
+                grassFrie(y, x, XYArray, append, threshCrop, visited, queue)
+            else:
+                visited[x][y] = 1
+
+
+# GrassFire algorithmen til at finde sorte pixels,
+# som tilhøre en større gruppe af sorte pixels
+def grassFrie(y, x, XYArray, append, threshCrop, visited, queue):
+
+    if append == True:
+        visited[x][y] = 1
+        XYArray.append([x, y])
+        queue.append([x, y])
+    append = True
+
+    if x < threshCrop.shape[1] and visited[x + 1][y] != 1 and threshCrop[y][x + 1] == 0:
+        grassFrie(y, x + 1, XYArray, append, threshCrop, visited, queue)
+
+    elif y < threshCrop.shape[0] and visited[x][y + 1] != 1 and threshCrop[y - 1][x] == 0:
+        grassFrie(y + 1, x, XYArray, append, threshCrop, visited, queue)
+
+    elif x > 0 and visited[x - 1][y] != 1 and threshCrop[y][x - 1] == 0:
+        grassFrie(y, x - 1, XYArray, append, threshCrop, visited, queue)
+
+    elif y > 0 and visited[x][y - 1] != 1 and threshCrop[y - 1][x] == 0:
+        grassFrie(y - 1, x, XYArray, append, threshCrop, visited, queue)
+
+
+    elif len(queue) != 0:
+        append = False
+        x, y = queue.pop()
+        grassFrie(y, x, XYArray, append, threshCrop, visited, queue)
+
+    else:
+        print('jeg er færdig nu')
+        xArray, yArray = zip(*XYArray)
+
+        maxX = max(xArray)
+        maxY = max(yArray)
+
+        minX = min(xArray)
+        minY = min(yArray)
+
+        cv2.rectangle(nummerpladeFrame, (minX, minY), (maxX, maxY), (0, 255, 0), 2)
+        letter = threshCrop[minY:minX,  maxY:maxX]
+        cv2.imshow('letter', letter)
+
+
+
+#Load billede og konvater det til gray-scale
+#cap = cv2.VideoCapture(0)
+
+#while True:
+    # Capture frame-by-frame
+    #ret, frame = cap.read()
+
+#indlæs billede
+frame = cv2.imread('testnummerplader.jpg')
+
+#grayScaler billede
+grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#tiltøj et blur filter
 grayImg = cv2.bilateralFilter(grayImg, 13, 15, 15)
 
 #edge detection
@@ -22,23 +92,42 @@ cnt = contours[max_index]
 
 #Finder højden og breden af den rektangel, som contouren tegner
 x, y, w, h = cv2.boundingRect(cnt) #Læser mere om dette
-cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 #Laver et frame kun til nummerpladen
 #og fylder det med et croped billede
-nummerpladeFrame = img[y: y + h, x: x + w]
+nummerpladeFrame = frame[y: y + h, x: x + w]
 
-#Laver et binart billede af det croped billede
-grayCrop = cv2.cvtColor(nummerpladeFrame, cv2.COLOR_BGR2GRAY)
-_, threshCrop = cv2.threshold(grayCrop, 150, 255, cv2.THRESH_BINARY_INV)
+threshCrop = np.zeros((nummerpladeFrame.shape[0], nummerpladeFrame.shape[1]), dtype=np.uint8)
 
-#finder tegn og samler det i en string og printer
+for x in range(nummerpladeFrame.shape[0]):
+    for y in range(nummerpladeFrame.shape[1]):
+        r, g, b = nummerpladeFrame[x, y]
+        gray = (r / 3 + g / 3 + b / 3)
+
+        if gray > 70:
+            threshCrop[x, y] = 255
+        else:
+            threshCrop[x, y] = 0
+
+height = 50
+width = int(height * 4.75)
+dim = (width, height)
+# resize image
+resized = cv2.resize(threshCrop, dim, interpolation = cv2.INTER_AREA)
+count(resized)
+
+# finder tegn og samler det i en string og printer
 out_below = pt.image_to_string(threshCrop)
-print("output: ", out_below)
+print(out_below)
 
-#Viser de forskellige billeder udervejes
-cv2.imshow('original', img)
-cv2.imshow('threshCrop', threshCrop)
-cv2.imshow('crop', nummerpladeFrame)
-
+cv2.imshow('original', frame)
+cv2.imshow('canny', edged)
+cv2.imshow('nummplade frame', nummerpladeFrame)
+cv2.imshow('thresh nummerplade', threshCrop)
 cv2.waitKey(0)
+
+
+
+#if cv2.waitKey(1) & 0xFF == ord('q'):
+    #break
